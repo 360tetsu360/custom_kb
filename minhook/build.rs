c1c55e115fd2585198cb9e4f962d665eb7a292da
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2022-02-20 13:23:30 +0900

    Minecraft Version 1.18.2

--- minhook/build.rs
+++ minhook/build.rs
@@ -3,5 +3,4 @@ use std::env;
 fn main() {
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     println!("cargo:rustc-link-search=native={}/native", project_dir);
-    println!("cargo:rustc-link-lib=static=libMinHook");
 }
